0ec5010e9a7ada928218958ab3a41caeb1f8c19a
innurov1992Albert <albert.zinnurov2017@yandex.ru>
2021-08-09 11:31:01 +0200

    create new layout Settings and init this

--- app/src/main/java/com/example/youtubehome/SettingsActivity.java
+++ app/src/main/java/com/example/youtubehome/SettingsActivity.java
@@ -1,14 +1,33 @@
 package com.example.youtubehome;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button buttonSaveInformation;
+    private EditText userNameEdT, userNickEdT, userStatusEdT;
+    private CircleImageView imageViewNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        init();
+    }
+
+    private void init() {
+        buttonSaveInformation = (Button) findViewById(R.id.button_save_for_settings);
+
+        userNameEdT = (EditText) findViewById(R.id.set_user_name);
+        userNickEdT = (EditText) findViewById(R.id.set_user_nick);
+        userStatusEdT = (EditText) findViewById(R.id.save_user_information);
+
+        imageViewNew = (CircleImageView) findViewById(R.id.profile_image);
     }
 }
